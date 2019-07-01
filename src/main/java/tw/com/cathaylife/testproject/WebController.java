843cea3f8e61597010008a27c1b9156364904074
JinYan <jinyan@cathaylife.com.tw>
2019-07-01 14:04:23 +0800

    add Skaffold

---
+++ src/main/java/tw/com/cathaylife/testproject/WebController.java
@@ -0,0 +1,13 @@
+package tw.com.cathaylife.testproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
