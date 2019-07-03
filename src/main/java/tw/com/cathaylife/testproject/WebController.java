2417c27f1e56f871ac9b2ac1ca1ea59be2d1e8a1
JinYan <jinyan@cathaylife.com.tw>
2019-07-03 17:03:55 +0800

    update

--- src/main/java/tw/com/cathaylife/testproject/WebController.java
+++ src/main/java/tw/com/cathaylife/testproject/WebController.java
@@ -1,8 +1,12 @@
 package tw.com.cathaylife.testproject;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,23 +22,72 @@ public class WebController {
 
     @RequestMapping("/execMySQL")
     @ResponseBody
-    public Map<String, String> execMySQL(@RequestParam String mysql_sql, @RequestParam String db2_sql, @RequestParam String exec_count) {
-        Map<String, String> resultMap = new HashMap<String, String>();
-        resultMap.put("exec_time", "32.000");
+    public Map<String, Object> execMySQL(@RequestParam String mysql_sql, @RequestParam String db2_sql,
+            @RequestParam String exec_count) {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://mysql-stateful-0.mysql.test-namespace.svc.cluster.local/mycat");
+        dataSource.setUsername("ebaf");
+        dataSource.setPassword("ebaf");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List test_data = jdbcTemplate.queryForList("select POLICY_NO from dtab0001_ta limit " + exec_count);
+
+        long start = System.currentTimeMillis();
+        Iterator it = test_data.iterator();
+        int count = 0;
+        while (it.hasNext()) {
+            Map<String, String> map = (Map<String, String>) it.next();
+            String POLICY_NO = map.get("POLICY_NO");
+            System.err.println("POLICY_NO = " + POLICY_NO);
+            count += jdbcTemplate.queryForList(mysql_sql, new Object[] {POLICY_NO}).size();
+        }
+        long end = System.currentTimeMillis();
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("exec_time", (end - start) + " ms");
+        resultMap.put("result", "總筆數：" + count);
         return resultMap;
     }
 
     @RequestMapping("/execMyCat")
     @ResponseBody
-    public Map<String, String> execMyCat(@RequestParam String mysql_sql, @RequestParam String db2_sql, @RequestParam String exec_count) {
-        Map<String, String> resultMap = new HashMap<String, String>();
-        resultMap.put("exec_time", "32.000");
+    public Map<String, Object> execMyCat(@RequestParam String mysql_sql, @RequestParam String db2_sql,
+            @RequestParam String exec_count) {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://mycat-service.test-namespace.svc.cluster.local:8066/mycat");
+        dataSource.setUsername("ebaf");
+        dataSource.setPassword("ebaf");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List test_data = jdbcTemplate.queryForList("select POLICY_NO from dtab0001_ta limit " + exec_count);
+
+        long start = System.currentTimeMillis();
+        Iterator it = test_data.iterator();
+        int count = 0;
+        while (it.hasNext()) {
+            Map<String, String> map = (Map<String, String>) it.next();
+            String POLICY_NO = map.get("POLICY_NO");
+            System.err.println("POLICY_NO = " + POLICY_NO);
+            count += jdbcTemplate.queryForList(mysql_sql, new Object[] {POLICY_NO}).size();
+        }
+        long end = System.currentTimeMillis();
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("exec_time", (end - start) + " ms");
+        resultMap.put("result", "總筆數：" + count);
         return resultMap;
     }
 
     @RequestMapping("/execDB2")
     @ResponseBody
-    public Map<String, String> execDB2(@RequestParam String mysql_sql, @RequestParam String db2_sql, @RequestParam String exec_count) {
+    public Map<String, String> execDB2(@RequestParam String mysql_sql, @RequestParam String db2_sql,
+            @RequestParam String exec_count) {
         Map<String, String> resultMap = new HashMap<String, String>();
         resultMap.put("exec_time", "32.000");
         return resultMap;
@@ -42,9 +95,10 @@ public class WebController {
 
     @RequestMapping("/execTest")
     @ResponseBody
-    public Map<String, String> execTest(@RequestParam String mysql_sql, @RequestParam String db2_sql, @RequestParam String exec_count) {
+    public Map<String, String> execTest(@RequestParam String mysql_sql, @RequestParam String db2_sql,
+            @RequestParam String exec_count) {
         Map<String, String> resultMap = new HashMap<String, String>();
-        resultMap.put("exec_time", "32.000");
+        resultMap.put("exec_time", "33.000");
         return resultMap;
     }
 }
