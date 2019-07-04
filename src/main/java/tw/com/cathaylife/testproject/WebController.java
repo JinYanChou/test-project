3db7e6b802d37f534a5bcb2bfd3bc53d7e00c6aa
JinYan <jinyan@cathaylife.com.tw>
2019-07-04 11:42:28 +0800

    update

--- src/main/java/tw/com/cathaylife/testproject/WebController.java
+++ src/main/java/tw/com/cathaylife/testproject/WebController.java
@@ -20,6 +20,38 @@ public class WebController {
         return "index";
     }
 
+    @RequestMapping("/execLocal")
+    @ResponseBody
+    public Map<String, Object> execLocal(@RequestParam String mysql_sql, @RequestParam String db2_sql,
+            @RequestParam String exec_count) {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1/mycat?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("cathaylife");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List test_data = jdbcTemplate.queryForList("select POLICY_NO from dtab0001_ta limit " + exec_count);
+
+        long start = System.currentTimeMillis();
+        Iterator it = test_data.iterator();
+        int count = 0;
+        while (it.hasNext()) {
+            Map<String, String> map = (Map<String, String>) it.next();
+            String POLICY_NO = map.get("POLICY_NO");
+            System.err.println("POLICY_NO = " + POLICY_NO);
+            count += jdbcTemplate.queryForList(mysql_sql, new Object[] {POLICY_NO}).size();
+        }
+        long end = System.currentTimeMillis();
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("exec_time", (end - start) + " ms");
+        resultMap.put("result", "總筆數：" + count);
+        return resultMap;
+    }
+
     @RequestMapping("/execMySQL")
     @ResponseBody
     public Map<String, Object> execMySQL(@RequestParam String mysql_sql, @RequestParam String db2_sql,
@@ -27,9 +59,9 @@ public class WebController {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://mysql-stateful-0.mysql.test-namespace.svc.cluster.local/mycat");
-        dataSource.setUsername("ebaf");
-        dataSource.setPassword("ebaf");
+        dataSource.setUrl("jdbc:mysql://mysql-stateful-0.mysql.test-namespace.svc.cluster.local/mycat?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("cathaylife");
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
@@ -59,7 +91,7 @@ public class WebController {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://mycat-service.test-namespace.svc.cluster.local:8066/mycat");
+        dataSource.setUrl("jdbc:mysql://mycat-service.test-namespace.svc.cluster.local:8066/mycat?useSSL=false");
         dataSource.setUsername("ebaf");
         dataSource.setPassword("ebaf");
 
